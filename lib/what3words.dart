4a8d67b5f38debdde009af30d28df3e93cfdabec
Jeroen Trzaska <jeroen@trzaska.xyz>
2021-06-15 23:36:41 +0200

    Update what3words.dart

--- lib/what3words.dart
+++ lib/what3words.dart
@@ -11,8 +11,11 @@ export 'src/request/coordinate.dart';
 export 'src/request/grid_section_request.dart' hide GridSectionRequest;
 
 export 'src/response/api_error.dart';
+
 export 'src/response/api_response.dart';
+export 'src/response/response.dart';
 export 'src/response/autosuggest.dart';
+export 'src/response/autosuggest_options.dart';
 export 'src/response/grid_section.dart';
 export 'src/response/language.dart';
 export 'src/response/location.dart';
