5001961ff86ccc7f9ede7ec874d343cf13944465
Manuela Sabatino <mn.sabatino@gmail.com>
2024-08-14 17:26:04 +0100

    add the locales to the available languages and update the test

--- lib/src/response/language.dart
+++ lib/src/response/language.dart
@@ -15,8 +15,14 @@ class Languages {
   String nativeName;
   String code;
   String name;
+  List<Locale>? locales;  // Add the list of locales
 
-  Languages({required this.nativeName, required this.code, required this.name});
+  Languages({
+    required this.nativeName,
+    required this.code,
+    required this.name,
+    this.locales,  // Include locales in the constructor
+  });
 
   factory Languages.fromJson(Map<String, dynamic> json) =>
       _$LanguagesFromJson(json);
@@ -24,7 +30,24 @@ class Languages {
   Map<String, dynamic> toJson() => _$LanguagesToJson(this);
 }
 
-//toJson, fromJson
+/// Represents a specific locale within a language
+class Locale {
+  String nativeName;
+  String code;
+  String name;
+
+  Locale({
+    required this.nativeName,
+    required this.code,
+    required this.name,
+  });
+
+  factory Locale.fromJson(Map<String, dynamic> json) => _$LocaleFromJson(json);
+
+  Map<String, dynamic> toJson() => _$LocaleToJson(this);
+}
+
+// toJson, fromJson for Language
 Language _$LanguageFromJson(Map<String, dynamic> json) {
   return Language(
     languages: (json['languages'] as List<dynamic>)
@@ -34,20 +57,39 @@ Language _$LanguageFromJson(Map<String, dynamic> json) {
 }
 
 Map<String, dynamic> _$LanguageToJson(Language instance) => <String, dynamic>{
-  'languages': instance.languages.map((e) => e.toJson()).toList(),
-};
+      'languages': instance.languages.map((e) => e.toJson()).toList(),
+    };
 
+// toJson, fromJson for Languages
 Languages _$LanguagesFromJson(Map<String, dynamic> json) {
   return Languages(
+    nativeName: json['nativeName'] as String,
+    code: json['code'] as String,
+    name: json['name'] as String,
+    locales: (json['locales'] as List<dynamic>?)
+        ?.map((e) => Locale.fromJson(e as Map<String, dynamic>))
+        .toList(),
+  );
+}
+
+Map<String, dynamic> _$LanguagesToJson(Languages instance) => <String, dynamic>{
+      'nativeName': instance.nativeName,
+      'code': instance.code,
+      'name': instance.name,
+      'locales': instance.locales?.map((e) => e.toJson()).toList(),
+    };
+
+// toJson, fromJson for Locale
+Locale _$LocaleFromJson(Map<String, dynamic> json) {
+  return Locale(
     nativeName: json['nativeName'] as String,
     code: json['code'] as String,
     name: json['name'] as String,
   );
 }
 
-Map<String, dynamic> _$LanguagesToJson(Languages instance) => <String, dynamic>{
-  'nativeName': instance.nativeName,
-  'code': instance.code,
-  'name': instance.name,
-};
-
+Map<String, dynamic> _$LocaleToJson(Locale instance) => <String, dynamic>{
+      'nativeName': instance.nativeName,
+      'code': instance.code,
+      'name': instance.name,
+    };
--- test/available_languages_test.dart
+++ test/available_languages_test.dart
@@ -6,10 +6,30 @@ void main() {
   var api = What3WordsV3(Platform.environment['W3W_API_KEY']!);
 
   test('testLanguages', () async {
-    var languages = await api
-        .availableLanguages()
-        .execute();
-    expect(languages.isSuccessful(), true);
-    languages.data()?.languages;
+    var response = await api.availableLanguages().execute();
+    expect(response.isSuccessful(), true);
+
+    var languages = response.data()?.languages;
+    expect(languages, isNotNull); // Ensure languages list is not null
+    expect(languages!.isNotEmpty, true); // Ensure the list is not empty
+
+    for (var language in languages) {
+      // Check that the basic properties are not null or empty
+      expect(language.name, isNotEmpty);
+      expect(language.code, isNotEmpty);
+      expect(language.nativeName, isNotEmpty);
+
+      // If locales are available, ensure they are valid
+      if (language.locales != null && language.locales!.isNotEmpty) {
+        for (var locale in language.locales!) {
+          expect(locale.name, isNotEmpty);
+          expect(locale.code, isNotEmpty);
+          expect(locale.nativeName, isNotEmpty);
+        }
+      } else {
+        // Optionally check that locales can be null or an empty list
+        expect(language.locales, anyOf(isNull, isEmpty));
+      }
+    }
   });
 }
